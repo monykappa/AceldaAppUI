867aaff382c49085eee27290f6fd371092a7eff1
Kim Ouddommony <kimmony039@gmail.com>
2024-03-19 12:34:26 +0700

    navigation host done

--- app/src/main/java/com/example/acledaapp/MainActivity.kt
+++ app/src/main/java/com/example/acledaapp/MainActivity.kt
@@ -1,28 +1,15 @@
 package com.example.acledaapp
 
 import android.os.Bundle
-import androidx.compose.foundation.layout.*
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.text.KeyboardOptions
 import com.example.acledaapp.ui.theme.AcledaAppTheme
-import androidx.compose.material.*
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.*
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.acledaapp.screens.mainScreen
-import com.example.acledaapp.screens.detailScreen
-import com.example.acledaapp.screens.mainScreen
+import com.example.acledaapp.screens.bakongScreen
 import com.example.acledaapp.screens.mainScreen
 
 
@@ -31,14 +18,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AcledaAppTheme {
-                mainScreen()
+                homeSceen()
                 }
             }
         }
     }
 
 
+@Preview(showBackground = true)
+@Composable
+fun app() {
+    homeSceen()
+}
 
+@Composable
+fun homeSceen() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            mainScreen(navController = navController)
+        }
+        composable("detail") {
+            bakongScreen()
+        }
+    }
+}
 
 
 
--- app/src/main/java/com/example/acledaapp/screens/main_screen.kt
+++ app/src/main/java/com/example/acledaapp/screens/main_screen.kt
@@ -6,92 +6,41 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.Image
-import android.os.Bundle
-import androidx.navigation.NavController
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.Color.Companion.Cyan
-import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.rememberImagePainter
 //import com.example.acledaapp.ui.theme.AcledaAppTheme
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.*
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.scale
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color.Companion.Black
-import androidx.compose.ui.graphics.Color.Companion.Gray
-import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Image
-import androidx.compose.ui.text.font.FontVariation.width
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.max
-import coil.annotation.ExperimentalCoilApi
-import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter.State.Empty.painter
-import com.example.acledaapp.ui.theme.AcledaAppTheme
 import androidx.compose.ui.text.font.FontFamily
+import androidx.navigation.NavController
 import com.example.acledaapp.R
-import com.example.acledaapp.screens.mainScreen
-import com.example.acledaapp.ui.theme.AcledaAppTheme
-import com.example.acledaapp.ui.theme.Purple40
+import androidx.navigation.NavHostController
 
 
 val BlueBackground = Color(0xFF2c446a)
@@ -114,8 +63,6 @@ val quicksandFontFamily = FontFamily(
 val poppinsFontFamily = FontFamily(
     Font(R.font.font_poppins),
 )
-
-
 
 data class BalanceItem(
     val imageResourceId: Int,
@@ -176,84 +123,131 @@ val favoriteItems = listOf(
 
 )
 
-
-@Preview(showSystemUi = true)
 @Composable
-fun mainScreen() {
+fun mainScreen(navController: NavController) {
     Column(modifier = Modifier.fillMaxSize()) {
-        Surface(
-            modifier = Modifier
-                .height(85.dp)
-                .fillMaxWidth(),
-            color = Color(0xFF25395A)
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                IconButton(onClick = { /* Handle menu icon click */ }) {
-                    // Replace R.drawable.menu_icon with your actual icon resource
-                    Image(
-                        painter = painterResource(id = R.drawable.menu_icon),
-                        contentDescription = null,
-                        modifier = Modifier.size(20.dp)
-                    )
-                }
+        //Top Navigation Bar
+        TopNavBar(navController)
 
-                Image(
-                    painter = painterResource(id = R.drawable.aclogo),
-                    contentDescription = null,
-                    modifier = Modifier.size(150.dp)
-                )
-
-                Spacer(modifier = Modifier.weight(1f))
-
-                IconButton(onClick = { /* Handle notification icon click */ }) {
-                    // Replace R.drawable.ic_notification with your actual icon resource
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_notification),
-                        contentDescription = null,
-                        modifier = Modifier.size(20.dp)
-                    )
-                }
-
-                Spacer(modifier = Modifier.width(0.dp))
-
-                IconButton(onClick = { /* Handle dollar icon click */ }) {
-                    // Replace R.drawable.unnamed with your actual icon resource
-                    Image(
-                        painter = painterResource(id = R.drawable.unnamed),
-                        contentDescription = null,
-                        modifier = Modifier.size(30.dp)
-                    )
-                }
-            }
-        }
-
+        //Menu Section
         MenuSection(menuRow1, height = 118.dp)
         MenuSection(menuRow2, height = 120.dp)
         MenuSection(menuRow3, height = 122.dp)
 
-
+        //Favorites container boxes
         favoriteBoxes(favoriteItems)
 
+        //Bottom background
+        BottomBackground()
+    }
+}
 
 
-        Surface(
-            modifier = Modifier.fillMaxSize().offset(y = 0.dp),
-            color = Color(0xFF21324E)
+
+@Composable
+fun TopNavBar(navController: NavController) {
+    Surface(
+        modifier = Modifier
+            .height(85.dp)
+            .fillMaxWidth(),
+        color = Color(0xFF25395A)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            // Replace R.drawable.image with your actual image resource
+            //menu icons
+            IconButton(onClick = { /* Handle menu icon click */ }) {
+                Image(
+                    painter = painterResource(id = R.drawable.menu_icon),
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+
             Image(
-                painter = painterResource(id = R.drawable.image),
+                painter = painterResource(id = R.drawable.aclogo),
                 contentDescription = null,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier.size(150.dp)
             )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            IconButton(onClick = { /* Handle notification icon click */ }) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_notification),
+                    contentDescription = null,
+                    modifier = Modifier.size(20.dp)
+                )
+            }
+
+            Spacer(modifier = Modifier.width(0.dp))
+
+            IconButton(onClick = {
+                // Navigate to the "detail" destination (bakong.kt screen)
+                navController.navigate("detail")
+            }) {
+                Image(
+                    painter = painterResource(id = R.drawable.bakong),
+                    contentDescription = null,
+                    modifier = Modifier.size(30.dp)
+                )
+            }
         }
     }
 }
+
+
+@Composable
+fun MenuSection(balanceItems: List<BalanceItem>, height: Dp) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(height),
+        color = Color(0xFF21324E)
+    ) {
+        Row(
+            modifier = Modifier
+                .background(color = Color(0xFF21324E))
+                .bottomBorderColor(color = Color(0xFF476CB0), borderWidth = 1.dp)
+        ) {
+            balanceItems.forEach { item ->
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(top = 20.dp),
+                ) {
+                    IconButton(onClick = { /* Handle button click */ }) {
+                        Column(
+                            horizontalAlignment = Alignment.CenterHorizontally,
+
+                            ) {
+                            Image(
+                                painter = painterResource(id = item.imageResourceId),
+                                contentDescription = item.text,
+                                modifier = Modifier.size(40.dp)
+                            )
+                            Text(
+                                item.text,
+                                textAlign = TextAlign.Center,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 14.sp,
+                                color = Color.White,
+                                modifier = Modifier.padding(top = 8.dp)
+                            )
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+            }
+        }
+    }
+}
+
+
 
 @Composable
 fun favoriteBoxes(favoriteItems: List<FavoriteItem>) {
@@ -328,54 +322,19 @@ fun favoriteBoxes(favoriteItems: List<FavoriteItem>) {
 }
 
 
-
 @Composable
-fun MenuSection(balanceItems: List<BalanceItem>, height: Dp) {
+fun BottomBackground() {
     Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(height),
+        modifier = Modifier.fillMaxSize().offset(y = 0.dp),
         color = Color(0xFF21324E)
     ) {
-        Row(
-            modifier = Modifier
-                .background(color = Color(0xFF21324E))
-                .bottomBorderColor(color = Color(0xFF476CB0), borderWidth = 1.dp)
-        ) {
-            balanceItems.forEach { item ->
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(top = 20.dp),
-                ) {
-                    IconButton(onClick = { /* Handle button click */ }) {
-                        Column(
-                            horizontalAlignment = Alignment.CenterHorizontally,
-
-                        ) {
-                            Image(
-                                painter = painterResource(id = item.imageResourceId),
-                                contentDescription = item.text,
-                                modifier = Modifier.size(40.dp)
-                            )
-                            Text(
-                                item.text,
-                                textAlign = TextAlign.Center,
-                                fontWeight = FontWeight.Normal,
-                                fontSize = 14.sp,
-                                color = Color.White,
-                                modifier = Modifier.padding(top = 8.dp)
-                            )
-                        }
-                    }
-                }
-                Spacer(modifier = Modifier.width(16.dp))
-            }
-        }
+        Image(
+            painter = painterResource(id = R.drawable.image),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
-
 
 
 
